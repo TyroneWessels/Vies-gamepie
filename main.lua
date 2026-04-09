c472754f77b7284e0075ea761033683a45c8ecfa
JonVer403 <jcverhoeven@outlook.com>
2026-04-09 13:45:48 +0200

    test new

--- main.lua
+++ main.lua
@@ -177,8 +177,8 @@ function love.load()
     gameState = STATE.MENU
     initGame()
 
-    menuMusic = love.audio.newSource("moonlight.mp3", "stream")
-    gameMusic = love.audio.newSource("playing.mp3", "stream")
+    menuMusic = love.audio.newSource("music/moonlight.mp3", "stream")
+    gameMusic = love.audio.newSource("music/playing.mp3", "stream")
     menuMusic:setLooping(true)
     gameMusic:setLooping(true)
 
